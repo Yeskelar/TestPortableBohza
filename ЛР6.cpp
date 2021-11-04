125a4090ec6f30ae974486ecc2561085281dbce6
Alex Bohza <bogza.alex2004@gmail.com>
2021-11-04 16:24:04 +0200

    Фіксація змін

---
+++ ЛР6.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include "windows.h"
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	float x, y, h, a, b;
+ cout << "Введіть a: ";
+ cin >> a;
+ cout << "Введіть b: ";
+ cin >> b;
+ cout << "Введіть x: ";
+ cin >> x;
+ cout << "Введіть h: ";
+ cin >> h;
+ //do
+	//{
+	// y = sin(x) + abs(x) + pow(2, x);
+	// cout << setw(10) << "Y = " << setw(5) << y << endl;
+	// x += h;
+	//}
+ //while (x <= b);
+ while (x <= b)
+ {
+	 y = sin(x) + abs(x) + pow(2, x);
+	 cout << setw(10) << "Y = " << setw(5) << y << endl;
+	 x += h;
+ }
+ return 0;
+}
